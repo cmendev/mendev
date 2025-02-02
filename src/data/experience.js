e520297f38e30941c6ed70c16428f922c5255b12
AndresMGDev <cmendoza022002@gmail.com>
2025-02-02 15:42:30 -0500

    Add dynamic pages for experience.

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,26 +1,34 @@
+import { TAGS } from "./tags";
+
 export const EXPERIENCE = [
   {
     date: "July 2024 - December 2024",
     title: "PL/SQL Developer",
+    id: 'plsql-developer-sonda',
     company: "Sonda de Colombia S.A",
     description:
-      "Developed solutions using PL/SQL, SQL Developer, and Oracle APEX, optimizing business processes.",
+      "As a PL/SQL Developer at Sonda de Colombia S.A, I was responsible for designing and implementing complex database solutions using PL/SQL, SQL Developer, and Oracle APEX. I led several initiatives to optimize critical business processes, including performance tuning and query optimization. I also ensured data integrity and implemented solutions that significantly enhanced system performance across multiple applications, leading to a noticeable improvement in the efficiency of daily operations. My role included collaborating with cross-functional teams to develop tailored database solutions that aligned with business needs, as well as providing ongoing support to resolve database issues. Additionally, I actively participated in designing scalable solutions for long-term system growth, helping to future-proof the company’s data management systems.",
     link: "#",
+    tags: [TAGS.PLSQL, TAGS.JAVA, TAGS.CSS, TAGS.JAVASCRIPT, TAGS.APEX],
   },
   {
     date: "September 2023 - December 2023",
     title: "Support Agent",
+    id: 'support-agent-thomas-greg-&-sons',
     company: "Thomas Greg & Sons",
     description:
-      "Provided remote technical support, resolving open cases and training operators to prevent future issues.",
+      "In my role as a Support Agent at Thomas Greg & Sons, I provided remote technical support for customers, efficiently resolving complex technical issues and ensuring a smooth operational flow. I was responsible for managing and tracking open cases, delivering timely solutions, and assisting customers with troubleshooting steps. Furthermore, I provided training to new operators and offered guidance on system usage, helping to increase the overall efficiency of our team. My work also focused on enhancing system reliability by proactively identifying potential issues and implementing preventive measures. Through continuous process improvement and collaboration with other teams, I contributed to the enhancement of the company’s technical support strategy, ensuring higher customer satisfaction and smoother system operations.",
     link: "#",
+    tags: [],
   },
   {
     date: "May 2022 - November 2022",
     title: "Junior Developer",
+    id: 'junior-developer-sentecol',
     company: "Sentecol S.A.S",
     description:
-      "Implemented and optimized software solutions in the healthcare sector, improving processes with PHP, Laravel, and JavaScript.",
+      "During my time as a Junior Developer at Sentecol S.A.S, I played a key role in the redesign of the 'Fisasist' platform, a project that aimed to improve medical information management within the healthcare sector. This initiative greatly enhanced user experience by streamlining workflows and making critical data more accessible and actionable. I contributed extensively to the frontend development of 'SQBot,' a chatbot designed to improve client interaction and engagement. I worked on optimizing the UI performance, ensuring the application was responsive and user-friendly. Additionally, I provided technical support for various software solutions, maintaining a perfect track record of resolving issues and delivering solutions on time. Through these projects, I honed my skills in frontend technologies such as React, TypeScript, HTML, and TailwindCSS, while also gaining experience with Docker for containerization and deployment. My contributions helped drive project success and improve system performance, ultimately leading to better customer satisfaction and more efficient internal processes.",
     link: "#",
+    tags: [TAGS.TYPESCRIPT, TAGS.REACT, TAGS.LARAVEL, TAGS.HTML, TAGS.TAILWINDCSS, TAGS.DOCKER],
   },
 ];
