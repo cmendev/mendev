00c30210b4bd609c0330f1e95291427e034aa5aa
AndresMGDev <cmendoza022002@gmail.com>
2025-02-02 15:43:38 -0500

    Add Icons and gradient styles.

--- src/data/tags.js
+++ src/data/tags.js
@@ -1,69 +1,119 @@
-import Java from "../components/icons/technologies/Java.astro";
-import Spring from "../components/icons/technologies/Spring.astro";
+// 📦 Lenguajes y Superconjuntos
 import JavaScript from "../components/icons/technologies/JavaScript.astro";
-import React from "../components/icons/technologies/React.astro";
-import CSS from "../components/icons/technologies/CSS.astro";
-import HTML from "../components/icons/technologies/HTML.astro";
 import TypeScript from "../components/icons/technologies/TypeScript.astro";
+import Java from "../components/icons/technologies/Java.astro";
+import PLSQL from "../components/icons/technologies/Plsql.astro";
+
+// 🌐 Lenguajes de Marcado y Estilo
+import HTML from "../components/icons/technologies/HTML.astro";
+import CSS from "../components/icons/technologies/CSS.astro";
+
+// ⚡ Frameworks y Librerías
+import React from "../components/icons/technologies/React.astro";
 import Nextjs from "../components/icons/technologies/Nextjs.astro";
-import TailwindCSS from "../components/icons/technologies/TailwindCSS.astro";
+import Spring from "../components/icons/technologies/Spring.astro";
 import Thymeleaf from "../components/icons/technologies/Thymeleaf.astro";
+import Laravel from "../components/icons/technologies/Laravel.astro";
+import Apex from "../components/icons/technologies/Apex.astro";
+import TailwindCSS from "../components/icons/technologies/TailwindCSS.astro";
+
+// 🛠️ Herramientas y Control de Versiones
 import Git from "../components/icons/technologies/Git.astro";
+import Docker from "../components/icons/technologies/Docker.astro";
+import PostgreSQL from "../components/icons/technologies/PostgreSQL.astro";
 
 export const TAGS = {
+  // 📦 Lenguajes y Superconjuntos
   JAVASCRIPT: {
     name: "JavaScript",
-    class: "text-yellow-500/80 bg-white",
+    class: "from-yellow-500/80 to-white dark:to-neutral-950",
     icon: JavaScript,
-  },
-  REACT: {
-    name: "React",
-    class: "text-blue-500/80 bg-white",
-    icon: React,
-  },
-  CSS: {
-    name: "CSS",
-    class: "text-blue-400/80 bg-white",
-    icon: CSS,
-  },
-  HTML: {
-    name: "HTML",
-    class: "text-orange-500/80 bg-white",
-    icon: HTML,
   },
   TYPESCRIPT: {
     name: "TypeScript",
-    class: "text-blue-600/80 bg-white",
+    class: "from-blue-600/80 to-white dark:to-neutral-950",
     icon: TypeScript,
+  },
+
+  // ⚡ Frameworks y Librerías basados en JS/TS
+  REACT: {
+    name: "React",
+    class: "from-blue-500/80 to-white dark:to-neutral-950",
+    icon: React,
   },
   NEXTJS: {
     name: "Nextjs",
-    class: "text-black/80 bg-white",
+    class: "from-black/80 to-white dark:to-neutral-950",
     icon: Nextjs,
   },
+
+  // 🌐 Lenguajes de Marcado y Estilo
+  HTML: {
+    name: "HTML",
+    class: "from-orange-500/80 to-white dark:to-neutral-950",
+    icon: HTML,
+  },
+  CSS: {
+    name: "CSS",
+    class: "from-blue-400/80 to-white dark:to-neutral-950",
+    icon: CSS,
+  },
   TAILWINDCSS: {
-    name: "Tailwindcss",
-    class: "text-teal-500/80 bg-white",
+    name: "TailwindCSS",
+    class: "from-teal-500/80 to-white dark:to-neutral-950",
     icon: TailwindCSS,
   },
+
+  // 📦 Otros Lenguajes
   JAVA: {
     name: "Java",
-    class: "text-orange-500/80 bg-white",
+    class: "from-orange-500/80 to-white dark:to-neutral-950",
     icon: Java,
   },
+  PLSQL: {
+    name: "PL/SQL",
+    class: "from-yellow-500/80 to-white dark:to-neutral-950",
+    icon: PLSQL,
+  },
+
+  // ⚡ Frameworks relacionados con Java
   SPRING: {
     name: "Spring",
-    class: "text-[#6cb43c] bg-white",
+    class: "from-[#6cb43c] to-white dark:to-neutral-950",
     icon: Spring,
   },
   THYMELEAF: {
     name: "Thymeleaf",
-    class: "text-green-500/80 bg-white",
+    class: "from-green-500/80 to-white dark:to-neutral-950",
     icon: Thymeleaf,
   },
+
+  // ⚡ Frameworks de Backend
+  LARAVEL: {
+    name: "Laravel",
+    class: "from-red-500/80 to-white dark:to-neutral-950",
+    icon: Laravel,
+  },
+  APEX: {
+    name: "Apex",
+    class: "from-purple-500/80 to-white dark:to-neutral-950",
+    icon: Apex,
+  },
+
+  // 🛠️ Herramientas y Control de Versiones
   GIT: {
     name: "Git",
-    class: "text-red-500/80 bg-white",
+    class: "from-red-500/80 to-white dark:to-neutral-950",
     icon: Git,
+  },
+  DOCKER: {
+    name: "Docker",
+    class: "from-blue-500/80 to-white dark:to-neutral-950",
+    icon: Docker,
+  },
+  POSTGRESQL: {
+    name: "PostgreSQL",
+    class: "from-blue-300/80 to-white dark:to-neutral-950",
+    icon: PostgreSQL,
   },
 };
